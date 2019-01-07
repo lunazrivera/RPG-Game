ead2c30be09b24b04868cbdd1fcb04df5ef50d82
Luna <luna.z.rivera@gmail.com>
2019-01-06 17:47:49 -0800

    "Audio enabled on click"

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,10 +1,4 @@
 $(document).ready(function () {
-// ----------------------------------------
-/*The funciton above will execute once the document
-is ready to run the javascript. Here we'l set up the audio
-to be played*/
-// ---------------------------------------- 
-
 
 // ----------------------------------------
 /*This function makes the body's height,
@@ -14,12 +8,15 @@ function windowH() {
     var wH = $(window).height();
     var wW = $(window).width();
     $('body').css({height: wH});
- } windowH();
+    } windowH();
+
+
 // ----------------------------------------
 // Global bindings | variables
 // ----------------------------------------
-var gameInitialized = false;
+var audioElement = document.getElementById("audio-fantasy");
 var startPage;
+
 
 
 
@@ -33,21 +30,24 @@ var startPage;
 and turned off the display of the start-page and turned on
 the display for character-page. */
 $("#play-button").on("click", function() {
-    gameInitialized = true;
+    playAudio();
     startPage = $("#start-page").css("display", "none");
-    
     if (startPage) {
         $("#character-page").css("display", "inline-block");
-    };
+    }
 });
-
-
 
 
 // ----------------------------------------
 // Function Grounds
 // ----------------------------------------
-
+function playAudio() { 
+    audioElement.play(); 
+  } 
+  
+  function pauseAudio() { 
+    audioElement.pause(); 
+  } 
 
 
 });
